ec355680e911a0bd77b70fb6d1e769191f2cfe37
Bakhaow <safwanechahboune@gmail.com>
2025-03-03 03:40:02 +0100

    Visual changes on navigation & admin menu

--- project/backend/routes/googleRoutes.js
+++ project/backend/routes/googleRoutes.js
@@ -1,10 +1,7 @@
 import express from "express";
-import { OAuth2Client } from "google-auth-library";
-import { authenticate } from "../middlewares/authMiddleware.js";
 import { googleAuth } from "../controllers/googleController.js";
 
 const router = express.Router();
-const client = new OAuth2Client(process.env.VITE_GOOGLE_CLIENT_ID);
 
 router.post("/", googleAuth);
 
